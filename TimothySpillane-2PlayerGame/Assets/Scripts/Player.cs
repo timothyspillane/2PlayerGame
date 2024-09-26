2e8f6443e6337e8ff0845f4be5668e380f89f149
Rentheapple <90929021+timothyspillane@users.noreply.github.com>
2024-09-25 17:30:48 -0700

    2PlayerGame-Main
    - September 25th 2024
     - Added a moveable box
     - designed a simple level one
     - made variables for speed/jump speed

--- TimothySpillane-2PlayerGame/Assets/Scripts/Player.cs
+++ TimothySpillane-2PlayerGame/Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] KeyCode playerUp;
     [SerializeField] KeyCode playerRight;
     [SerializeField] KeyCode playerLeft;
+    [SerializeField] float speed;
+    [SerializeField] float jumpSpeed;
+
 
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -21,20 +25,20 @@ public class Player : MonoBehaviour
         if (Input.GetKey(playerUp))
         {
             LayerMask layers = LayerMask.GetMask("Ground");
-            RaycastHit2D cast = Physics2D.Raycast(transform.position, Vector2.down, .51f, layers);
+            RaycastHit2D cast = Physics2D.Raycast(transform.position, Vector2.down, .61f, layers);
             if (cast)
             {
-                rb.velocity = new Vector2(rb.velocity.x, 5f);
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             }
             
         }
         if (Input.GetKey(playerRight))
         {
-            rb.velocity = new Vector2(5f, rb.velocity.y);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
         if (Input.GetKey(playerLeft))
         {
-            rb.velocity = new Vector2(-5f, rb.velocity.y);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
         }
     }
 }
