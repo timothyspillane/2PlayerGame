207622e90b66214f637367e4d31b70e3b46f684e
Rentheapple <90929021+timothyspillane@users.noreply.github.com>
2024-10-09 17:27:58 -0700

    TimothySpillane-2PGame
    - october 9th 2024 (my birthday!!!!)
    
    - added win screen
    - added menu
    - added notes
    - added new scene functions in gamemanager

--- TimothySpillane-2PlayerGame/Assets/Scripts/GameManager.cs
+++ TimothySpillane-2PlayerGame/Assets/Scripts/GameManager.cs
@@ -5,13 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] GameObject WinPanel;
     // Update is called once per frame
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.R))
@@ -21,6 +20,25 @@ public class GameManager : MonoBehaviour
         if(null == GameObject.FindWithTag("Box"))
         {
             Debug.Log("You Won!");
+            Victory();
+
         }
+    }
+    private void Victory()
+    {
+        WinPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // When you get to the last level, a bug will occur
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
